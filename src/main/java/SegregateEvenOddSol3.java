198272a93a62807d8c9d0be8e2dd9fd46787d978
divyajha <dparashar94@gmail.com>
2023-05-24 23:02:34 +0530

    https://www.geeksforgeeks.org/segregate-even-odd-numbers-set-3/
    Solution 3

--- src/main/java/SegregateEvenOddSol3.java
+++ src/main/java/SegregateEvenOddSol3.java
@@ -3,31 +3,34 @@ package src.main.java;
 public class SegregateEvenOddSol3 {
 
     public static void main(String[] args) {
-        int[] arr = {1, 9, 5, 3, 2, 6, 7, 11};
+        int[] arr = {1, 9, 4, 10, 7, 5, 3, 2, 6, 7, 11}; //
         int n = arr.length;
         segregateBest(arr, n);
+        for (int j : arr) {
+            System.out.println(j);
+        }
     }
 
     private static void segregateBest(int[] arr, int n) {
-        int[] finalArray = new int[n];
         int temp;
         int end = n-1;
         int start = 0;
-
-        for(int i=0; i< n; i++){
-            if(arr[i] %2 == 0 ){
-                finalArray[start] = arr[i];
+        while(start < end){
+            if(!isEven(arr[start]) && isEven(arr[end])){
+                temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
                 start++;
-            } else {
-                //need to swap both the value, arr[i] value in final[i] and final[i] in arr[i]
-
-                temp =arr[i];
-                arr[i] = finalArray[end];
-                finalArray[end] = temp;
-                end --;
+                end--;
+            } else if(isEven(arr[start])){
+                start++;
+            } else if(!isEven(arr[end])){
+                end--;
             }
         }
-        for(int i = 0; i< n; i++)
-        System.out.println(finalArray[i]);
+
+    }
+    private static boolean isEven(int k){
+        return k%2 ==0;
     }
 }
